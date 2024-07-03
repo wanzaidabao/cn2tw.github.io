a86635a00b6d54e9491118f3aedbb865435f1ec2
Jim <jim@pve1.twbest.biz>
2024-07-03 22:20:14 +0800

    Erick@worker01,20240703 22:20

--- src/.vuepress/.temp/internal/routes.js
+++ src/.vuepress/.temp/internal/routes.js
@@ -1,7 +1,8 @@
 export const redirects = JSON.parse("{}")
 
 export const routes = Object.fromEntries([
-  ["/", { loader: () => import(/* webpackChunkName: "index.html" */"/data/home/gitrepo/cn2tw.online/src/.vuepress/.temp/pages/index.html.js"), meta: {"t":"项目主页","i":"home"} }],
+  ["/", { loader: () => import(/* webpackChunkName: "index.html" */"/data/home/gitrepo/cn2tw.online/src/.vuepress/.temp/pages/index.html.js"), meta: {"t":"大陆旅游","i":"home"} }],
+  ["/example.html", { loader: () => import(/* webpackChunkName: "example.html" */"/data/home/gitrepo/cn2tw.online/src/.vuepress/.temp/pages/example.html.js"), meta: {"t":"项目主页","i":"home"} }],
   ["/portfolio.html", { loader: () => import(/* webpackChunkName: "portfolio.html" */"/data/home/gitrepo/cn2tw.online/src/.vuepress/.temp/pages/portfolio.html.js"), meta: {"t":"档案主页","i":"home"} }],
   ["/demo/", { loader: () => import(/* webpackChunkName: "demo_index.html" */"/data/home/gitrepo/cn2tw.online/src/.vuepress/.temp/pages/demo/index.html.js"), meta: {"t":"主要功能与配置演示","i":"laptop-code"} }],
   ["/demo/disable.html", { loader: () => import(/* webpackChunkName: "demo_disable.html" */"/data/home/gitrepo/cn2tw.online/src/.vuepress/.temp/pages/demo/disable.html.js"), meta: {"t":"布局与功能禁用","i":"gears","O":4} }],
@@ -9,27 +10,10 @@ export const routes = Object.fromEntries([
   ["/demo/layout.html", { loader: () => import(/* webpackChunkName: "demo_layout.html" */"/data/home/gitrepo/cn2tw.online/src/.vuepress/.temp/pages/demo/layout.html.js"), meta: {"t":"布局","i":"object-group","O":2} }],
   ["/demo/markdown.html", { loader: () => import(/* webpackChunkName: "demo_markdown.html" */"/data/home/gitrepo/cn2tw.online/src/.vuepress/.temp/pages/demo/markdown.html.js"), meta: {"t":"Markdown 展示","i":"fab fa-markdown","O":2} }],
   ["/demo/page.html", { loader: () => import(/* webpackChunkName: "demo_page.html" */"/data/home/gitrepo/cn2tw.online/src/.vuepress/.temp/pages/demo/page.html.js"), meta: {"t":"页面配置","i":"file","O":3} }],
-  ["/guide/", { loader: () => import(/* webpackChunkName: "guide_index.html" */"/data/home/gitrepo/cn2tw.online/src/.vuepress/.temp/pages/guide/index.html.js"), meta: {"t":"指南","i":"lightbulb"} }],
-  ["/guide/foo/", { loader: () => import(/* webpackChunkName: "guide_foo_index.html" */"/data/home/gitrepo/cn2tw.online/src/.vuepress/.temp/pages/guide/foo/index.html.js"), meta: {"t":"Foo 功能","i":"lightbulb"} }],
-  ["/guide/foo/ray.html", { loader: () => import(/* webpackChunkName: "guide_foo_ray.html" */"/data/home/gitrepo/cn2tw.online/src/.vuepress/.temp/pages/guide/foo/ray.html.js"), meta: {"t":"Ray","i":"circle-info"} }],
+  ["/guide/", { loader: () => import(/* webpackChunkName: "guide_index.html" */"/data/home/gitrepo/cn2tw.online/src/.vuepress/.temp/pages/guide/index.html.js"), meta: {"t":"大陆旅游指南2","i":"lightbulb"} }],
   ["/guide/bar/", { loader: () => import(/* webpackChunkName: "guide_bar_index.html" */"/data/home/gitrepo/cn2tw.online/src/.vuepress/.temp/pages/guide/bar/index.html.js"), meta: {"t":"Bar 功能","i":"lightbulb"} }],
   ["/guide/bar/baz.html", { loader: () => import(/* webpackChunkName: "guide_bar_baz.html" */"/data/home/gitrepo/cn2tw.online/src/.vuepress/.temp/pages/guide/bar/baz.html.js"), meta: {"t":"Baz","i":"circle-info"} }],
+  ["/guide/foo/", { loader: () => import(/* webpackChunkName: "guide_foo_index.html" */"/data/home/gitrepo/cn2tw.online/src/.vuepress/.temp/pages/guide/foo/index.html.js"), meta: {"t":"Foo 功能","i":"lightbulb"} }],
+  ["/guide/foo/ray.html", { loader: () => import(/* webpackChunkName: "guide_foo_ray.html" */"/data/home/gitrepo/cn2tw.online/src/.vuepress/.temp/pages/guide/foo/ray.html.js"), meta: {"t":"Ray","i":"circle-info"} }],
   ["/404.html", { loader: () => import(/* webpackChunkName: "404.html" */"/data/home/gitrepo/cn2tw.online/src/.vuepress/.temp/pages/404.html.js"), meta: {"t":""} }],
 ]);
-
-if (import.meta.webpackHot) {
-  import.meta.webpackHot.accept()
-  if (__VUE_HMR_RUNTIME__.updateRoutes) {
-    __VUE_HMR_RUNTIME__.updateRoutes(routes)
-  }
-  if (__VUE_HMR_RUNTIME__.updateRedirects) {
-    __VUE_HMR_RUNTIME__.updateRedirects(redirects)
-  }
-}
-
-if (import.meta.hot) {
-  import.meta.hot.accept(({ routes, redirects }) => {
-    __VUE_HMR_RUNTIME__.updateRoutes(routes)
-    __VUE_HMR_RUNTIME__.updateRedirects(redirects)
-  })
-}
--- src/.vuepress/.temp/internal/siteData.js
+++ src/.vuepress/.temp/internal/siteData.js
@@ -1,14 +1,1 @@
-export const siteData = JSON.parse("{\"base\":\"/\",\"lang\":\"zh-CN\",\"title\":\"文档演示\",\"description\":\"vuepress-theme-hope 的文档演示\",\"head\":[],\"locales\":{}}")
-
-if (import.meta.webpackHot) {
-  import.meta.webpackHot.accept()
-  if (__VUE_HMR_RUNTIME__.updateSiteData) {
-    __VUE_HMR_RUNTIME__.updateSiteData(siteData)
-  }
-}
-
-if (import.meta.hot) {
-  import.meta.hot.accept(({ siteData }) => {
-    __VUE_HMR_RUNTIME__.updateSiteData(siteData)
-  })
-}
+export const siteData = JSON.parse("{\"base\":\"/\",\"lang\":\"zh-CN\",\"title\":\"湾湾的二岸笔记\",\"description\":\"台湾人到大陆的生活指南GoPass\",\"head\":[],\"locales\":{}}")
